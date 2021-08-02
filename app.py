521fee9f545e16e8df45140967abbcd56f95d703
jungeun919 <renee8369@likelion.org>
2021-08-02 17:02:34 +0900

    코드정리

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Blueprint('app', __name__)
 configuration_path = "./cfg/yolov3.cfg"
 weights_path = "./yolov3.weights"
 
-labels = open("./data/coco.names").read().strip().split('\n')  # list of names
+labels = open("./data/coco.names").read().strip().split('\n')
 
 # Setting minimum probability to eliminate weak predictions
 probability_minimum = 0.5
@@ -28,15 +28,9 @@ threshold = 0.3
 network = cv2.dnn.readNetFromDarknet(configuration_path, weights_path)
 
 # Getting names of all layers
-layers_names_all = network.getLayerNames()  # list of layers' names
+layers_names_all = network.getLayerNames()
 layers_names_output = [layers_names_all[i[0] - 1] for i in network.getUnconnectedOutLayers()]  # list of layers' names
 
-# Check point
-
-
-# app = Flask(__name__)
-
-# app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
 
 
@@ -44,7 +38,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def html(content):  # Also allows you to set your own <head></head> etc
+def html(content):
    return '<html><head></head><body>' + content + '</body></html>'
 
 @app.route('/barcode', methods=['GET'])
@@ -66,7 +60,6 @@ def barcode():
                 used_codes.append(barcode_num)
                 time.sleep(5)
 
-                # 추가
                 product = db.barcode.find_one({'barcode': barcode_num})
                 print(product)
                 name = product['name']
@@ -76,10 +69,8 @@ def barcode():
                 fat = product['fat(g)']
                 cholesterol = product['cholesterol(mg)']
                 protein = product['protein(g)']
-                print(name, calories, sodium, carbohydrate, fat, cholesterol, protein)
                 return render_template('home/barcode.html', name=name, calories=calories, sodium=sodium,
                                         carbohydrate=carbohydrate, fat=fat, cholesterol=cholesterol, protein=protein)
-                # 추가
 
             elif code.data.decode('utf-8') in used_codes:
                 print('Sorry, this code has been already used')
@@ -98,18 +89,6 @@ def index():
 def camera():
     return render_template('home/cam.html')
 
-# @app.route('/camera', methods=['GET', 'POST'])
-# def camera():
-#     if request.method == 'POST':
-#         name = request.args.get('name')
-#         image_url = request.form['imageString']
-#         print(image_url)
-
-#         file_path = os.path.join('./static/images/input', image_url)
-#         image_url.save(file_path)
-#         return render_template('camera.html', user_image = 'images/output/' + image_url)
-#     return render_template('cam.html')
-
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
     return render_template('home/upload.html')
@@ -126,18 +105,10 @@ def predict():
                 pathImage = file_path
                 image_input = img = cv2.imread(pathImage)
                 image_input_shape = image_input.shape
-                # Check point
-                
-                
+      
                 blob = cv2.dnn.blobFromImage(image_input, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-
-                # Check point
-                
-                # Check point
                 # Slicing blob and transposing to make channels come at the end
-                blob_to_show = blob[0, :, :, :].transpose(1, 2, 0)
-                
-                
+                blob_to_show = blob[0, :, :, :].transpose(1, 2, 0)           
                 
                 network.setInput(blob)  # setting blob as input to the network
                 start = time.time()
@@ -150,14 +121,10 @@ def predict():
                 # randint(low, high=None, size=None, dtype='l')
                 colors = np.random.randint(0, 255, size=(len(labels), 3))
                 
-                # Check point
-                
                 bounding_boxes = []
                 confidences = []
                 class_numbers = []
                 h, w = image_input_shape[:2]  # Slicing from tuple only first two elements
-                
-                # Check point
                 
                 for result in output_from_network:
                     # Going through all detections from current output layer
@@ -165,13 +132,10 @@ def predict():
                         # Getting class for current object
                         scores = detection[5:]
                         class_current = np.argmax(scores)
-                
                         confidence_current = scores[class_current]
-                
-                        if confidence_current > probability_minimum:
-                            
+
+                        if confidence_current > probability_minimum:    
                             box_current = detection[0:4] * np.array([w, h, w, h])
-                
                             x_center, y_center, box_width, box_height = box_current.astype('int')
                             x_min = int(x_center - (box_width / 2))
                             y_min = int(y_center - (box_height / 2))
@@ -206,25 +170,13 @@ def predict():
                         text_box_current = '{}: {:.4f}'.format(labels[int(class_numbers[i])], confidences[i])
                 
                         cv2.putText(image_input, text_box_current, (x_min, y_min - 7), cv2.FONT_HERSHEY_SIMPLEX,
-                                    1.5, colour_box_current, 5)
-                # plt.rcParams['figure.figsize'] = (10.0, 10.0)
-                # image_output = cv2.cvtColor(image_input, cv2.COLOR_BGR2RGB)
-                # cv2.imshow(image_output)     
+                                    1.5, colour_box_current, 5)   
                 cv2.imwrite('./static/images/output/' + filename , image_input)
-                # plt.show()
-                #plt.savefig('static/images/plot.jpg')
-                
-                
                 return render_template('home/predict.html', products = list(set(objects)), user_image = 'images/output/' + filename)
-                # return render_template('predict.html', products = list(set(objects)), user_image = filename)
                 
         except Exception as e:
             return "Unable to read the file. Please check if the file extension is correct."
 
-# if __name__ == "__main__":
-#     app.run(debug = True, use_reloader = False)
-    
-    
 
 '''
  <form action="http://127.0.0.1:5000/" onLoad="LoadOnce()">
